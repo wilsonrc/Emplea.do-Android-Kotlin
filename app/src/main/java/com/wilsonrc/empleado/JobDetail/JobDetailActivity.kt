f5cc81974ac94205ace4e884a533bf505da8c5e8
Wilsonrc <wilsonrc16@gmail.com>
2018-03-12 13:15:34 -0400

    Add share job offer to job detail activity

--- app/src/main/java/com/wilsonrc/empleado/JobDetail/JobDetailActivity.kt
+++ app/src/main/java/com/wilsonrc/empleado/JobDetail/JobDetailActivity.kt
@@ -18,12 +18,48 @@ import com.wilsonrc.empleado.utils.ActivityUtils
 
 import kotlinx.android.synthetic.main.activity_job_detail.*
 import android.net.Uri.fromParts
-
-
-
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 
 class JobDetailActivity : AppCompatActivity() , JobDetailContract.View {
+
+    lateinit var job : Job
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.new_game -> {
+                shareJobOffer(job)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.job_detail_menu, menu)
+        return true
+    }
+
+    override fun showProgressBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hideProgressBar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_detail)
+        job = intent.extras.getParcelable<Job>("JobObject")
+        val presenter = JobDetailPresenter()
+        presenter.attach(this)
+        presenter.setJobDetail(job)
+    }
 
     override fun showJobDetail(job: Job) {
         with(job){
@@ -38,11 +74,9 @@ class JobDetailActivity : AppCompatActivity() , JobDetailContract.View {
             textHowApply.text = howToApply
 
             buttonApplyNow.setOnClickListener{
-                val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto", companyEmail, null))
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, title)
-                startActivity(Intent.createChooser(emailIntent, "Send your Application"))
+                applyNow(job)
             }
+
             Glide.with(this@JobDetailActivity).load(companyLogoUrl).into(imageViewCompanyLogo)
             if(isRemote != null && isRemote as Boolean){
                 textRemote.visibility = View.VISIBLE
@@ -52,26 +86,20 @@ class JobDetailActivity : AppCompatActivity() , JobDetailContract.View {
         }
     }
 
-    override fun showProgressBar() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun applyNow(job: Job){
+        val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", job.companyEmail, null))
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, job.title)
+        startActivity(Intent.createChooser(emailIntent, "Send your Application"))
+    }
+    private fun shareJobOffer(job: Job){
+        val shareBody = "${job.title}: \\n ${job.description} \\n\\n ${job.howToApply} \\n Company Info: *${job.companyName} \\n *${job.companyEmail} \\n ${job.companyUrl}"
+        val sharingIntent = Intent(android.content.Intent.ACTION_SEND)
+        sharingIntent.type = "text/plain"
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, job.title)
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody)
+        startActivity(Intent.createChooser(sharingIntent, "Share this offer"))
     }
 
-    override fun hideProgressBar() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_job_detail)
-
-
-
-        val job = intent.extras.getParcelable<Job>("JobObject")
-        val presenter = JobDetailPresenter()
-        presenter.attach(this)
-        presenter.setJobDetail(job)
-
-    }
 
 }
